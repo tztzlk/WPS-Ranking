51e22f9f96d8986163e868e23af34839b11c306a
iopwisp <@.com>
2025-07-31 00:39:28 +0500

    added structure

---
+++ backend/parser-service/src/main/java/cube/parser_service/ParserServiceApplication.java
@@ -0,0 +1,13 @@
+package cube.parser_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParserServiceApplication.class, args);
+	}
+
+}
---
+++ backend/parser-service/src/test/java/cube/parser_service/ParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cube.parser_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
