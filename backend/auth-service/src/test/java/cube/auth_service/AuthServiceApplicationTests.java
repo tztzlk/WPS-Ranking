854ef312e4b580cc96ffc4e8a53de15983319814
iopwisp <@.com>
2025-07-31 00:39:28 +0500

    added structure

---
+++ backend/auth-service/src/test/java/cube/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cube.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/leaderboard-service/src/main/java/cube/leaderboard_service/LeaderboardServiceApplication.java
@@ -0,0 +1,13 @@
+package cube.leaderboard_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardServiceApplication.class, args);
+	}
+
+}
